c26cb78b42b6dbf6b947e33f4c1bba8d1406a0b3
Bruce Dunn <bruceraymonddunn@gmail.com>
2026-06-07 14:14:32 +0930

    Added Navigation bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'screens/catch_list_screen.dart';
+import 'screens/add_catch_screen.dart';
+import 'screens/settings_screen.dart';
 import 'theme.dart';
 
 void main() {
@@ -14,7 +16,55 @@ class BragmatApp extends StatelessWidget {
     return MaterialApp(
       title: 'Bragmat',
       theme: AppTheme.lightTheme,
-      home: const CatchListScreen(),
+      home: const MainScreen(),
+    );
+  }
+}
+
+class MainScreen extends StatefulWidget {
+  const MainScreen({super.key});
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  int _selectedIndex = 0;
+
+  final List<Widget> _screens = [
+    const CatchListScreen(),
+    const AddCatchScreen(),
+    const SettingsScreen(),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _screens[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.list),
+            label: 'My Catches',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.add),
+            label: 'Add Catch',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: 'Settings',
+          ),
+        ],
+      ),
     );
   }
 }
